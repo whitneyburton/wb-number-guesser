0ce92b4184989eb235322ebf0130307d03cbfd25
Sally Haefling <sallyhaefling@gmail.com>
2018-10-13 15:30:11 -0600

    Fix bugs for all error messages

--- main.js
+++ main.js
@@ -17,110 +17,85 @@ userGuessNumber.addEventListener('keyup', function() {
   console.log(userGuessNumber.value);
   if (userGuessNumber.value === "") { 
   }})
-// error message here!
 
-submitButton.addEventListener('click', function() {
-  if(checkGuessIsNumber()) {
-    document.querySelector('.nan-error-message').innerHTML = `<i class="fas fa-exclamation-triangle"></i> Not a number`;
-  } else if(checkInputNotEmpty()) {
-  }  else {
-      submitGuess();
+submitButton.addEventListener('click', function(e) {
+  e.preventDefault();
+  if (checkInputNotEmpty()) {
+  } else {
+    submitGuess();
   }
 });
 
-updateButton.addEventListener('click', function() {
-	min = parseInt(document.querySelector("#min-range").value);
-	max = parseInt(document.querySelector("#max-range").value);
-	range = max - min;
-	randomNumber = generateRandomNumber(range, min);
-	replaceMinMax();
+updateButton.addEventListener('click', function(e) {
+  e.preventDefault();
+  min = parseInt(document.querySelector("#min-range").value);
+  max = parseInt(document.querySelector("#max-range").value);
+  range = max - min;
+  randomNumber = generateRandomNumber(range, min);
+  replaceMinMax();
   checkInputNotEmpty();
+  checkMinMaxValue();
 });
 
 resetButton.addEventListener('click', function() {
-	minEntry.value = "";
-	maxEntry.value = "";
-	userGuessNumber.value = "";
+  minEntry.value = "";
+  maxEntry.value = "";
+  userGuessNumber.value = "";
   generateRandomNumber(range, min);
   rightSideTextClear();
 });
 
-// random number max/min parameters 
-// Math.floor(Math.random() * (maxEntry - minEntry+1)) + minEntry;
-
 function checkInputNotEmpty() {
-  if (minEntry.value === "" && maxEntry === "") {
+  if (minEntry.value === "") {
     document.querySelector('.min-error-message').innerHTML = `<i class="fas fa-exclamation-triangle"></i> Enter a min range`;
-    document.querySelector('.max-error-message').innerHTML = `<i class="fas fa-exclamation-triangle"></i> Enter a max range`;
   }
-  else if (minEntry.value === "") {
-    document.querySelector('.min-error-message').innerHTML = `<i class="fas fa-exclamation-triangle"></i> Enter a min range`;
-    console.log("there's no min!")
-  } else if (maxEntry.value === "") {
-    console.log("there's no max!")
-    // console.log("error message");
+  if (maxEntry.value === "") {
     document.querySelector('.max-error-message').innerHTML = `<i class="fas fa-exclamation-triangle"></i> Enter a max range`;
   };
 };
 
 function generateRandomNumber(range, min) {
-	var updatedRandomNumber = Math.floor(Math.random() * (range + 1) + min);
-	console.log(`random number has been updated to ${updatedRandomNumber}`);
-	return updatedRandomNumber;
+  var updatedRandomNumber = Math.floor(Math.random() * (range + 1) + min);
+  console.log(`random number has been updated to ${updatedRandomNumber}`);
+  return updatedRandomNumber;
 };
 
 function replaceMinMax() {
-	document.querySelector('.min-entry').innerText = minEntry.value;
-	document.querySelector('.max-entry').innerText = maxEntry.value;
+  document.querySelector('.min-entry').innerText = minEntry.value;
+  document.querySelector('.max-entry').innerText = maxEntry.value;
 };
 
 function rightSideTextClear() {
   numberGuessed.value = "";
-  // fix code below, SAVE FOR LATER ...submit guess button doesn't get new number
-  // after reset
   document.querySelector('.evaluation-feedback').innerText = "";
   document.querySelector('.number-returned').innerText = "";
 };
 
-// submit guess button 
-function submitGuess(e) {
-  // e.preventDefault();
+function submitGuess() {
   numberGuessed.innerText = userGuessNumber.value;
   evaluateGuess(userGuessNumber.value);
 };
 
-// finish this 
 function checkMinMaxValue() {
-  if(minEntry.value > maxEntry.value) {
-    // error message here!
-    console.log("min greater than max");
-  }
-}
+  if (minEntry.value > maxEntry.value) {
+    document.querySelector('.evaluation-feedback').innerText = 
+    "Sorry, your min entry is higher than your max entry, try again";
+  }};
 
-// explain to sally, change input type to number if time  
-function checkGuessIsNumber() {
-    console.log('def not a number');
-    // input fields are always strings until you parseInt
-    var inputNum = parseInt(userGuessNumber.value);
-    console.log(isNaN(inputNum));
-    return isNaN(inputNum);
-};
-
-
-function evaluateGuess(userGuessNumber) {
-if (userGuessNumber == randomNumber) {
-  document.querySelector('.evaluation-feedback').innerText = 
-  "BOOM, you got it!";
-} else if (userGuessNumber > randomNumber && userGuessNumber < maxEntry.value) {
-  document.querySelector('.evaluation-feedback').innerText = 
-  "Sorry, that is too high";
-} else if (userGuessNumber < randomNumber && userGuessNumber > minEntry.value) {
-  document.querySelector('.evaluation-feedback').innerText = 
-  "Sorry that is too low";
-} else { 
-  document.querySelector('.evaluation-feedback').innerText = "Sorry, that is outside of the range, try again";
-}
-};
-// } else (userGuessNumber < ) {
-//   document.querySelector('.evaluation-feedback').innerText 
-// }
+  function evaluateGuess(userGuessNumber) {
+    if (userGuessNumber === "") {
+      document.querySelector('.evaluation-feedback').innerText =
+      "Please enter a guess!";
+    } else if (userGuessNumber == randomNumber) {
+      document.querySelector('.evaluation-feedback').innerText = 
+      "BOOM, you got it!";
+    } else if (userGuessNumber > randomNumber && userGuessNumber <= maxEntry.value) {
+      document.querySelector('.evaluation-feedback').innerText = 
+      "Sorry, that is too high.";
+    } else if (userGuessNumber < randomNumber && userGuessNumber >= minEntry.value) {
+      document.querySelector('.evaluation-feedback').innerText = 
+      "Sorry that is too low.";
+    } else { 
+      document.querySelector('.evaluation-feedback').innerText = 
+      "Sorry, that is outside of the range, try again.";
+    }};
